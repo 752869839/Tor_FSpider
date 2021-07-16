eab74d481f0355c0e09711002bad90fb0b43313c
陈立志 <44913268+752869839@users.noreply.github.com>
2021-07-16 17:04:28 +0800

    Delete login directory

--- login/config.py
+++
@@ -1,9 +0,0 @@
-
-#proxy
-PROXIES = [
-    {"http": "108.61.203.227:9398"}
-]
-#自由国度账户
-pncld_username = 'yuanxiao'     #yida    #ningmeng   #fengmi    #guazi     #kele   #shuibei   #shijin
-#guoba    #xuebi    #pingguo   #jianpan    #baixiong   #tangyuan
-pncld_word = 'Yanjingxianpi123456789'
